8aacfd1178893c56064bd7add0d85488b3c4e548
kariDiazRugerio <karinadiazrugerio@gmail.com>
2024-11-05 12:41:32 -0600

    se agrega el proyecto

---
+++ src/main/java/com/soaint/demo/ejercicio1/config/classController.java
@@ -0,0 +1,48 @@
+package com.soaint.demo.ejercicio1.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/services")
+public class classController {
+
+    //Endpoint 1: Recibe un dato por path parameter
+    @GetMapping("endpoint1/path/{data}")
+    public ResponseEntity<Map<String, String>> getPathParam(@PathVariable String data){
+        Map<String, String> response = new HashMap<>();
+        //response.put("message", "Received path parameter");
+        response.put("data", data);
+        return ResponseEntity.ok(response);
+    }
+
+    //Endpoint 2: Recibe un parametro por query parameter
+    @GetMapping("endpoint2/query")
+    public ResponseEntity<Map<String, String>> getQueryParam(@RequestParam(required = false) String param){
+        if (param == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("error", "No query parameter provided"));
+    }
+    Map<String, String> response = new HashMap<>();
+        //response.put("message", "Received query parameter");
+        response.put("param", param);
+        return ResponseEntity.ok(response);
+}
+
+//Endpoint 3: Recibe un jSON por POST
+@PostMapping("endpoint3/post")
+public ResponseEntity<Map<String, Object>> postJson(@RequestBody Map<String, Object> data){
+    if (data == null || data.isEmpty()){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", "No JSON data received"));
+    }
+    Map<String, Object> response = new HashMap<>();
+    //response.put("message", "Received JSON data");
+    response.put("data", data);
+    return ResponseEntity.ok(response);
+}
+}
